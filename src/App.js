be48662ce5fb6d1136559a9f6016d5ea07556387
Ron Gilmour <rgilmour70@gmail.com>
2019-07-11 07:26:27 -0400

    changed favicon, removed API stuff

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import './App.css';
 import Classify from './components/classify/classify';
-
-const WRITE_API = './api/write.php';
 
 class App extends Component {
 
